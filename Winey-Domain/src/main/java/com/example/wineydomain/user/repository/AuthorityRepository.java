8eee73ba27fc1d32d03a2df32a011f7962772aec
swa07016 <swa07016@naver.com>
2023-07-08 21:37:50 +0900

    feat: WineRepository

--- Winey-Domain/src/main/java/com/example/wineydomain/user/repository/AuthorityRepository.java
+++ Winey-Domain/src/main/java/com/example/wineydomain/user/repository/AuthorityRepository.java
@@ -3,9 +3,7 @@ package com.example.wineydomain.user.repository;
 
 import com.example.wineydomain.user.entity.Authority;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface AuthorityRepository extends JpaRepository<Authority, Long> {
 
 }
