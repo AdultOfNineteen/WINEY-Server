53abfed3f9ddd287e01a3b49ddaeea159f5010e7
swa07016 <swa07016@naver.com>
2023-07-15 19:30:44 +0900

    refactor: lombok 어노테이션 추가 in UserResponse

--- Winey-API/src/main/java/com/example/wineyapi/user/dto/UserResponse.java
+++ Winey-API/src/main/java/com/example/wineyapi/user/dto/UserResponse.java
@@ -1,12 +1,25 @@
 package com.example.wineyapi.user.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 public class UserResponse {
 
+    @NoArgsConstructor
+    @Getter
+    @AllArgsConstructor
+    @Builder
     public static class LoginUser {
-
+        private String field;
     }
 
+    @NoArgsConstructor
+    @Getter
+    @AllArgsConstructor
+    @Builder
     public static class DeleteUser {
-
+        private String field;
     }
 }
