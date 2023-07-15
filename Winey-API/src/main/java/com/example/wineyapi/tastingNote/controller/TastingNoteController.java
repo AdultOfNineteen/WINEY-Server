58deef2c39b25ad41d5dbaa299edfe87d88eebb5
swa07016 <swa07016@naver.com>
2023-07-15 21:02:17 +0900

    feat: tasting note controller method signature

---
+++ Winey-API/src/main/java/com/example/wineyapi/tastingNote/controller/TastingNoteController.java
@@ -0,0 +1,37 @@
+package com.example.wineyapi.tastingNote.controller;
+
+import com.example.wineyapi.tastingNote.dto.TastingNoteResponse;
+import com.example.wineycommon.reponse.CommonResponse;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class TastingNoteController {
+
+    @GetMapping("/users/{userId}/tasting-notes/{noteId}")
+    public CommonResponse<TastingNoteResponse.TastingNoteDTO> getTastingNote(@PathVariable Long userId,
+                                                                             @PathVariable Long noteId) {
+        return null;
+    }
+
+    @GetMapping("/users/{userId}/tasting-notes")
+    public CommonResponse<TastingNoteResponse.TastingNoteListDTO> getTastingNoteList(@PathVariable Long userId) {
+        return null;
+    }
+
+    @PostMapping("/users/{userId}/tasting-notes")
+    public CommonResponse<TastingNoteResponse.CreateTastingNoteDTO> createTastingNote(@PathVariable Long userId) {
+        return null;
+    }
+
+    @PatchMapping("/users/{userId}/tasting-notes/{noteId}")
+    public CommonResponse<TastingNoteResponse.UpdateTastingNoteDTO> updateTastingNote(@PathVariable Long userId,
+                                                                                      @PathVariable Long noteId) {
+        return null;
+    }
+
+    @DeleteMapping("/users/{userId}/tasting-notes/{noteId}")
+    public CommonResponse<TastingNoteResponse.DeleteTastingNoteDTO> deleteTastingNote(@PathVariable Long userId,
+                                                                                      @PathVariable Long noteId) {
+        return null;
+    }
+}
