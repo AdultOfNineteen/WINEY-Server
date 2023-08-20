95d5045f564423c1bc073f55ccc718b6721e0abd
LimHyunwoo <81962309+imenuuu@users.noreply.github.com>
2023-08-20 16:58:48 +0900

    feat : queryDsl 설정

--- Winey-Domain/src/main/java/com/example/wineydomain/tastingNote/repository/TastingNoteRepository.java
+++ Winey-Domain/src/main/java/com/example/wineydomain/tastingNote/repository/TastingNoteRepository.java
@@ -1,7 +1,11 @@
 package com.example.wineydomain.tastingNote.repository;
 
 import com.example.wineydomain.tastingNote.entity.TastingNote;
+import com.example.wineydomain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TastingNoteRepository extends JpaRepository<TastingNote, Long> {
+import java.util.List;
+
+public interface TastingNoteRepository extends JpaRepository<TastingNote, Long>,TastingNoteCustomRepository {
+    List<TastingNote> findByUser(User user);
 }
