ba4013e0bd749935a05845af7bb181113f38113d
LimHyunwoo <81962309+imenuuu@users.noreply.github.com>
2023-09-08 01:56:37 +0900

    feat : 테이스팅 노트 작성 기본 로직 설정

--- Winey-API/src/main/java/com/example/wineyapi/tastingNote/controller/TastingNoteController.java
+++ Winey-API/src/main/java/com/example/wineyapi/tastingNote/controller/TastingNoteController.java
@@ -3,6 +3,10 @@ package com.example.wineyapi.tastingNote.controller;
 import com.example.wineyapi.tastingNote.dto.TastingNoteRequest;
 import com.example.wineyapi.tastingNote.dto.TastingNoteResponse;
 import com.example.wineycommon.reponse.CommonResponse;
+import com.example.wineydomain.user.entity.User;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -19,9 +23,11 @@ public class TastingNoteController {
         return null;
     }
 
-    @PostMapping("/users/{userId}/tasting-notes")
-    public CommonResponse<TastingNoteResponse.CreateTastingNoteDTO> createTastingNote(@PathVariable Long userId,
-                                                                                      @RequestBody TastingNoteRequest.CreateTastingNoteDTO request) {
+    @Deprecated
+    @RequestMapping(value = "/users/tasting-notes", consumes = {"multipart/form-data"}, method = RequestMethod.POST)
+    @Operation(summary = "테이스팅 노트 작성 API 입니다. multi-form data 형식입니다.", description = "테이스팅 노트 작성 API")
+    public CommonResponse<TastingNoteResponse.CreateTastingNoteDTO> createTastingNote(@Parameter(hidden = true) @AuthenticationPrincipal User user,
+                                                                                      @ModelAttribute TastingNoteRequest.CreateTastingNoteDTO request) {
         return null;
     }
 
--- Winey-API/src/main/java/com/example/wineyapi/tastingNote/dto/TastingNoteRequest.java
+++ Winey-API/src/main/java/com/example/wineyapi/tastingNote/dto/TastingNoteRequest.java
@@ -1,12 +1,48 @@
 package com.example.wineyapi.tastingNote.dto;
 
+import com.example.wineydomain.common.model.BaseEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 public class TastingNoteRequest {
 
     @Getter
+    @Setter
+    @AllArgsConstructor
+    @Builder
     public static class CreateTastingNoteDTO {
-        private String field;
+        private int vintage;
+
+        private double officialAlcohol;
+
+        private int price;
+
+        private String color;
+
+        private int sweetness;
+
+        private int acidity;
+
+        private int alcohol;
+
+        private int body;
+
+        private int tannin;
+
+        private int finish;
+
+        private String feelings;
+
+        private List<MultipartFile> multipartFiles;
+
+        private boolean buyAgain;
+
+        private int rating;
     }
 
     @Getter
