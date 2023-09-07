f52ec5463365645c3855ccb9e3ea0f6af4b47225
LimHyunwoo <81962309+imenuuu@users.noreply.github.com>
2023-09-07 22:26:07 +0900

    refactor: 에러코드 추가

--- Winey-API/src/main/java/com/example/wineyapi/wineTip/controller/WineTipController.java
+++ Winey-API/src/main/java/com/example/wineyapi/wineTip/controller/WineTipController.java
@@ -2,8 +2,10 @@ package com.example.wineyapi.wineTip.controller;
 
 import com.example.wineyapi.wineTip.dto.WineTipResponse;
 import com.example.wineyapi.wineTip.service.WineTipService;
+import com.example.wineycommon.annotation.ApiErrorCodeExample;
 import com.example.wineycommon.reponse.CommonResponse;
 import com.example.wineycommon.reponse.PageResponse;
+import com.example.wineydomain.user.exception.UserAuthErrorCode;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -22,6 +24,7 @@ public class WineTipController {
     private final WineTipService wineTipService;
     @GetMapping("")
     @Operation(summary = "WineTip 리스트 조회")
+    @ApiErrorCodeExample(UserAuthErrorCode.class)
     public CommonResponse<PageResponse<List<WineTipResponse.WineTipDto>>> getWineTip(
             @Parameter(description = "페이지", example = "0") @RequestParam(required = false, defaultValue = "0") Integer page,
             @Parameter(description = "페이지 사이즈", example = "10") @RequestParam(required = false, defaultValue = "10")  Integer size
