9d292905bf95b2138f7485a9847bc1d57aae7e52
LimHyunwoo <81962309+imenuuu@users.noreply.github.com>
2023-09-10 14:58:02 +0900

    refactor : swagger 구체화

--- Winey-API/src/main/java/com/example/wineyapi/tastingNote/dto/TastingNoteResponse.java
+++ Winey-API/src/main/java/com/example/wineyapi/tastingNote/dto/TastingNoteResponse.java
@@ -2,6 +2,7 @@ package com.example.wineyapi.tastingNote.dto;
 
 import com.example.wineydomain.wine.entity.Country;
 import com.example.wineydomain.wine.entity.WineType;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -16,16 +17,17 @@ public class TastingNoteResponse {
     @AllArgsConstructor
     @Builder
     public static class TastingNoteListDTO {
+        @Schema(name = "테이스팅 노트 id")
         private Long noteId;
-
+        @Schema(name = "와인 이름")
         private String wineName;
-
+        @Schema(name = "와인 생산지")
         private Country country;
-
+        @Schema(name = "평점")
         private int starRating;
-
+        @Schema(name = "재구매 유무")
         private boolean buyAgain;
-
+        @Schema(name = "와인 타입 RED,WHITE 등등")
         private WineType wineType;
     }
 
