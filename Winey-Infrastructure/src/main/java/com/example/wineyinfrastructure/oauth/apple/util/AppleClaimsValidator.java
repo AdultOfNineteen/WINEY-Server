c6af8f78af99fbd0e8570f30c983673132869464
swa07016 <swa07016@naver.com>
2023-10-19 17:26:10 +0900

    feat: 애플로그인 환경변수 추가

--- Winey-Infrastructure/src/main/java/com/example/wineyinfrastructure/oauth/apple/util/AppleClaimsValidator.java
+++ Winey-Infrastructure/src/main/java/com/example/wineyinfrastructure/oauth/apple/util/AppleClaimsValidator.java
@@ -1,32 +1,26 @@
 package com.example.wineyinfrastructure.oauth.apple.util;
 
 import io.jsonwebtoken.Claims;
-import lombok.Value;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AppleClaimsValidator {
 
-    private static final String NONCE_KEY = "nonce";
-
-    // TODO - 환경변수 추가하기
     private final String iss;
     private final String clientId;
-    private final String nonce;
 
     public AppleClaimsValidator(
             @Value("${oauth.apple.iss}") String iss,
-            @Value("${oauth.apple.client-id}") String clientId,
-            @Value("${oauth.apple.nonce}") String nonce
+            @Value("${oauth.apple.client-id}") String clientId
     ) {
         this.iss = iss;
         this.clientId = clientId;
-        this.nonce = EncryptUtils.encrypt(nonce);
     }
 
     public boolean isValid(Claims claims) {
         return claims.getIssuer().contains(iss) &&
-                claims.getAudience().equals(clientId) &&
-                claims.get(NONCE_KEY, String.class).equals(nonce);
+                claims.getAudience().equals(clientId);
     }
 }
