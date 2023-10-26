90624577c857c14e0ffc47fb46db1fcf95485fd9
LimHyunwoo <81962309+imenuuu@users.noreply.github.com>
2023-10-26 20:08:46 +0900

    refactor : 테이스팅 노트 필터 제거

--- Winey-API/src/main/java/com/example/wineyapi/tastingNote/convertor/TastingNoteConvertor.java
+++ Winey-API/src/main/java/com/example/wineyapi/tastingNote/convertor/TastingNoteConvertor.java
@@ -371,7 +371,6 @@ public class TastingNoteConvertor {
 
     private List<TastingNoteResponse.WineTypeFilter> wineTypesFilter(Map<WineType, Integer> wineCountByType, int size) {
         List<TastingNoteResponse.WineTypeFilter> wineTypeFilters = new ArrayList<>();
-        wineTypeFilters.add(new TastingNoteResponse.WineTypeFilter("전체", checkValue(size)));
         for (Map.Entry<WineType, Integer> entry : wineCountByType.entrySet()) {
             wineTypeFilters.add(new TastingNoteResponse.WineTypeFilter(entry.getKey().getName(), checkValue(entry.getValue())));
         }
@@ -380,7 +379,6 @@ public class TastingNoteConvertor {
 
     private List<TastingNoteResponse.CountryFilter> countryFilter(Map<Country, Integer> wineCountByCountry, int size) {
         List<TastingNoteResponse.CountryFilter> countryFilters = new ArrayList<>();
-        countryFilters.add(new TastingNoteResponse.CountryFilter("전체", checkValue(size)));
         for (Map.Entry<Country, Integer> entry : wineCountByCountry.entrySet()) {
             countryFilters.add(new TastingNoteResponse.CountryFilter(entry.getKey().getValue(), checkValue(entry.getValue())));
         }
