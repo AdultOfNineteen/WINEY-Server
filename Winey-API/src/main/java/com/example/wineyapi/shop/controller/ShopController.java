585b646cff7702770e86832ddeae4794084b833e
LimHyunwoo <81962309+imenuuu@users.noreply.github.com>
2023-11-09 20:26:28 +0900

    feat : ShopController 추가

---
+++ Winey-API/src/main/java/com/example/wineyapi/shop/controller/ShopController.java
@@ -0,0 +1,12 @@
+package com.example.wineyapi.shop.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/admin/shops")
+public class ShopController {
+
+}
