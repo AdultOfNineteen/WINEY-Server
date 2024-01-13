d97590c8fc5ecda74fd4a0f61fd5d9f5d71c1339
LimHyunwoo <81962309+imenuuu@users.noreply.github.com>
2024-01-13 16:31:42 +0900

    feat : 슬랙 알림 비동기 처리 (#70)

--- Winey-Common/src/main/java/com/example/wineycommon/config/AsyncConfig.java
+++ Winey-Common/src/main/java/com/example/wineycommon/config/AsyncConfig.java
@@ -67,4 +67,15 @@ public class AsyncConfig extends AsyncConfigurerSupport {
         executor.initialize();
         return executor;
     }
+
+    @Bean(name = "slack-notification")
+    public Executor postSlackNotification(){
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(10); // 기본적으로 실행 대기 중인 Thread 개수
+        executor.setMaxPoolSize(10); // 동시에 동작하는 최대 Thread 개수
+        executor.setQueueCapacity(500); // CorePool이 초과될때 Queue에 저장했다가 꺼내서 실행된다. (500개까지 저장함)
+        executor.setThreadNamePrefix("slack-notification-thread"); // Spring에서 생성하는 Thread 이름의 접두사
+        executor.initialize();
+        return executor;
+    }
 }
