10004d64b599d1d794c30a07c53afed6405a4806
LimHyunwoo <81962309+imenuuu@users.noreply.github.com>
2024-01-16 18:37:23 +0900

    refactor : Swagger 위도 경도 예시 변경 (#72)

--- Winey-API/src/main/java/com/example/wineyapi/shop/dto/ShopReq.java
+++ Winey-API/src/main/java/com/example/wineyapi/shop/dto/ShopReq.java
@@ -16,26 +16,26 @@ public class ShopReq {
     @ToString
     public static class MapFilterDto{
         @NotNull(message = "위도를 입력해주세요")
-        @Schema(description = "사용자 위도", example = "37.5099121")
+        @Schema(description = "사용자 위도", example = "37.510845")
         private double latitude;
 
-        @Schema(description = "사용자 경도", example = "127.059554")
+        @Schema(description = "사용자 경도", example = "127.081854")
         @NotNull(message = "경도를 입력해주세요")
         private double longitude;
 
         @NotNull(message = "좌측 상단 위도를 입력해주세요")
-        @Schema(description = "좌측 상단 위도", example = "37.5099121")
+        @Schema(description = "좌측 상단 위도", example = "37.8085577")
         private double leftTopLatitude;
 
-        @Schema(description = "좌층 상단 경도", example = "127.0845284")
+        @Schema(description = "좌층 상단 경도", example = "126.8960541")
         @NotNull(message = "좌측 상단 경도를 입력해주세요")
         private double leftTopLongitude;
 
         @NotNull(message = "우측 하단 위도를 입력해주세요")
-        @Schema(description = "우측 하단 위도", example = "37.5099121")
+        @Schema(description = "우측 하단 위도", example = "37.4340242")
         private double rightBottomLatitude;
 
-        @Schema(description = "우측 하단 경도", example = "127.0845284")
+        @Schema(description = "우측 하단 경도", example = "127.2157401")
         @NotNull(message = "우측 하단 경도를 입력해주세요")
         private double rightBottomLongitude;
     }
