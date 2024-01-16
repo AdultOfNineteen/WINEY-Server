935cacbb7d84130837f01f0e8728c0685226267e
LimHyunwoo <81962309+imenuuu@users.noreply.github.com>
2024-01-16 18:25:54 +0900

    refactor : 와인 샵 필터링 전략 패턴으로 리팩토링 (#72)

---
+++ Winey-API/src/main/java/com/example/wineyapi/shop/service/strategy/ShopListStrategy.java
@@ -0,0 +1,11 @@
+package com.example.wineyapi.shop.service.strategy;
+
+import java.util.List;
+
+import com.example.wineyapi.shop.dto.ShopCommand;
+import com.example.wineydomain.shop.repository.ShopRepository;
+
+public interface ShopListStrategy {
+
+	List<ShopRepository.ShopMapList> getShopList(ShopCommand.getMapCommandDTO commandDTO);
+}
