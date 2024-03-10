3d44bba863c734ee1dda4626b63fdea7dc9bc4c6
swa07016 <swa07016@naver.com>
2024-03-10 16:21:10 +0900

    fix: 회원탈퇴 에러 (ShopBookMark 삭제)

--- Winey-Domain/src/main/java/com/example/wineydomain/shop/repository/ShopBookMarkRepository.java
+++ Winey-Domain/src/main/java/com/example/wineydomain/shop/repository/ShopBookMarkRepository.java
@@ -1,9 +1,15 @@
 package com.example.wineydomain.shop.repository;
 
+import com.example.wineydomain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.wineydomain.shop.entity.BookMarkPk;
 import com.example.wineydomain.shop.entity.ShopBookMark;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ShopBookMarkRepository extends JpaRepository<ShopBookMark, BookMarkPk> {
+    @Query(value = "select s from ShopBookMark s where s.id.user = :user")
+    List<ShopBookMark> findByUser(User user);
 }
